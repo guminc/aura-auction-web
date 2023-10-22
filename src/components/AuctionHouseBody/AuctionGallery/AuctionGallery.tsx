e221bc8954244fefdc1b5e2506179920fec00540
DennisDv24 <DennisDv24@proton.me>
2023-10-22 15:43:27 +0200

    launch countdown

--- src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
+++ src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
@@ -2,17 +2,31 @@ import style from './AuctionGallery.module.css'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import { AuctionCard } from './AuctionCard'
 import { auctionsAtTheSameTime } from '../../../state/autoAuctionStore'
+import Countdown from 'react-countdown'
+import { useState } from 'react'
 
-export const AuctionGallery = () => (
-	<div id={style['auction-container']}>
-		{RNEA.range(0, auctionsAtTheSameTime - 1).map((_, i) => {
-			return <AuctionCard lineIndex={i} key={i} />
-		})}
+const launchDate = new Date(Date.UTC(2023, 9, 22, 17, 0, 0, 0))
 
-		<div id={style['placeholder-overlay']} data-prelaunch="true">
-			<div id={style['placeholder-counter-container']}>
-				<span>00:00:00</span>
-			</div>
-		</div>
-	</div>
-)
+export const AuctionGallery = () => {
+
+    const [showCounter, setShowCounter] = useState<boolean>(true)
+    const showPage = () => setShowCounter(false)
+
+    return (
+        <div id={style['auction-container']}>
+            {RNEA.range(0, auctionsAtTheSameTime - 1).map((_, i) => {
+                return <AuctionCard lineIndex={i} key={i} />
+            })}
+
+            <div id={style['placeholder-overlay']} data-prelaunch={showCounter}>
+                <div id={style['placeholder-counter-container']}>
+                    <Countdown
+                        date={launchDate}
+                        onComplete={showPage}
+                        daysInHours
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
