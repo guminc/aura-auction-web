91c652bd438ae2a27a523ca04d5abbb982186723
DennisDv24 <DennisDv24@proton.me>
2023-10-22 19:44:13 +0200

    cleaning countdown

--- src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
+++ src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
@@ -2,15 +2,15 @@ import style from './AuctionGallery.module.css'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import { AuctionCard } from './AuctionCard'
 import { auctionsAtTheSameTime } from '../../../state/autoAuctionStore'
-import Countdown from 'react-countdown'
 import { useState } from 'react'
 
-const launchDate = new Date(Date.UTC(2023, 9, 22, 17, 0, 0, 0))
+// const launchDate = new Date(Date.UTC(2023, 9, 22, 16, 50, 0, 0))
 
 export const AuctionGallery = () => {
 
     const [showCounter, setShowCounter] = useState<boolean>(true)
     const showPage = () => setShowCounter(false)
+
 
     return (
         <div id={style['auction-container']}>
@@ -18,13 +18,16 @@ export const AuctionGallery = () => {
                 return <AuctionCard lineIndex={i} key={i} />
             })}
 
-            <div id={style['placeholder-overlay']} data-prelaunch={showCounter}>
+            <div 
+                id={style['placeholder-overlay']}
+                data-prelaunch={false}
+            >
                 <div id={style['placeholder-counter-container']}>
-                    <Countdown
+                    {/*<Countdown
                         date={launchDate}
                         onComplete={showPage}
                         daysInHours
-                    />
+                    />*/}
                 </div>
             </div>
         </div>
