4c7f5ea1650896f6f86bd0c986c97b2a34ee9fc4
Dick Jones <jones.rrw@gmail.com>
2023-10-25 14:59:48 +1030

    replace preload overlay graphic

--- src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
+++ src/components/AuctionHouseBody/AuctionGallery/AuctionGallery.tsx
@@ -7,29 +7,25 @@ import { useState } from 'react'
 // const launchDate = new Date(Date.UTC(2023, 9, 22, 16, 50, 0, 0))
 
 export const AuctionGallery = () => {
+	const [showCounter, setShowCounter] = useState<boolean>(true)
+	const showPage = () => setShowCounter(false)
 
-    const [showCounter, setShowCounter] = useState<boolean>(true)
-    const showPage = () => setShowCounter(false)
+	return (
+		<div id={style['auction-container']}>
+			{RNEA.range(0, auctionsAtTheSameTime - 1).map((_, i) => {
+				return <AuctionCard lineIndex={i} key={i} />
+			})}
 
-
-    return (
-        <div id={style['auction-container']}>
-            {RNEA.range(0, auctionsAtTheSameTime - 1).map((_, i) => {
-                return <AuctionCard lineIndex={i} key={i} />
-            })}
-
-            <div 
-                id={style['placeholder-overlay']}
-                data-prelaunch={false}
-            >
-                <div id={style['placeholder-counter-container']}>
-                    {/*<Countdown
+			<div id={style['placeholder-overlay']} data-prelaunch={false}>
+				<div className={style['body']}></div>
+				<div id={style['placeholder-counter-container']}>
+					{/*<Countdown
                         date={launchDate}
                         onComplete={showPage}
                         daysInHours
                     />*/}
-                </div>
-            </div>
-        </div>
-    )
+				</div>
+			</div>
+		</div>
+	)
 }
